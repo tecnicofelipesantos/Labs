c8cf0ff2b7c44dc8b5e3969fb3fe1f8b2d318c39
tecnicofelipesantos <tecnicofelipesantos@gmail.com>
2020-05-15 21:34:50 -0300

    fist commit

---
+++ MeuProjeto/src/app/institucional/sobre/sobre.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sobre',
+  template: `
+    <p>
+      sobre works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class SobreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
